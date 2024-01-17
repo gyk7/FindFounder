f4d446c8b0f5003fed5504c10282f3fe7a906d7f
GayoungKim <gyk7@naver.com>
2024-01-17 09:01:17 +0900

    'heap'

---
+++ test.py
@@ -0,0 +1,36 @@
+from sys import stdin
+N = int(stdin.readline())
+asdf =[]
+result = []
+for i in range(N):
+    str_line = stdin.readline().split()
+    
+    if str_line[0] == "push":
+        asdf.append(str_line[1])
+    elif str_line[0] == "front":
+        if len(asdf) == 0 :
+            result.append(-1)
+        else :
+            result.append(int(asdf[0]))
+    elif str_line[0] == "back":
+        if len(asdf) == 0 :
+            result.append(-1)
+        else :
+            result.append(int(asdf[-1]))
+    elif str_line[0] == "size":
+        result.append(len(asdf))
+    elif str_line[0] == "pop" :
+        if len(asdf) == 0 :
+            result.append(-1)
+        else :
+            result.append(asdf[0])
+            del(asdf[0])
+    elif str_line[0] == "empty" :
+        if len(asdf) == 0 :
+            result.append(1)
+        else :
+            result.append(0)
+
+#test = "\n".join(list(map(str,result)))
+
+print("\n".join(map(str,result)))
