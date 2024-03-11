df90f6c427813e2e85d0e9e8ea2dec15b5690ba6
GayoungKim <gyk7@naver.com>
2024-03-11 20:32:59 +0900

    이진탐색

---
+++ 이분탐색.py
@@ -0,0 +1,19 @@
+# programmers 입국심사
+def solution(n, times):
+    answer = 0
+    left = 0
+    right = max(times) * n
+ 
+    while left < right:
+        mid = (left + right) // 2
+        cnt = 0
+ 
+        for i in times:
+            cnt += mid // i
+ 
+        if cnt >= n:
+            right = mid
+        else:
+            left = mid + 1
+ 
+    return left
